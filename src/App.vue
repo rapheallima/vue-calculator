faaea3c73c34f32b1f3a40277dc7a9e098d49722
Raphael Lima <raphaellima365@gmail.com>
2024-04-14 15:07:13 -0300

    finalizando projeto

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,63 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, ref } from 'vue';
+
+const nums1 = ref('')
+const nums2 = ref('')
+
+let numero1 = nums1;
+let numero2 = nums2;
+
+const estado = reactive({
+    operacao: '',
+})
+
+function capturaOp(evento) {
+    estado.operacao = evento.target.value
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="container">
+        <header class="p-5 mb-4 mt-4 bg-dark rounded-3">
+            <h1 class="text-danger text-center">Calculadora</h1>
+        </header>
+        <form class="form-group">
+            <div class="row mb-5">
+                <label>Escolha a operação</label>
+                <select class="form-control" @change="capturaOp">
+                    <option value="">Operações</option>
+                    <option value="soma">Soma</option>
+                    <option value="subtracao">Subtração</option>
+                    <option value="divisao">Divisão</option>
+                    <option value="multiplicacao">Multiplicação</option>
+                </select>
+            </div>
+            <div class="row p-5">
+                <div class="col-md-6">
+                    <label>Número 1</label>
+                    <input v-model="nums1" class="form-control" type="number" required placeholder="Digite um número" />
+                </div>
+                <div class="col-md-6">
+                    <label>Número 2</label>
+                    <input v-model="nums2" class="form-control" type="number" required placeholder="Digite um número" />
+                </div>
+            </div>
+            <div class="row">
+                <h2 class="text-center">Resultado</h2>
+                <h3 v-if="estado.operacao === 'soma'" class="text-center mt-4">{{ numero1 +
+                    numero2 }} <p class="mt-4">Você está somando</p>
+                </h3>
+                <h3 v-else-if="estado.operacao === 'subtracao'" class="text-center mt-4">{{
+                    numero1 - numero2 }} <p class="mt-4">Você está subtraindo</p>
+                </h3>
+                <h3 v-else-if="estado.operacao === 'divisao'" class="text-center mt-4">{{
+                    numero1 / numero2 }} <p class="mt-4">Você está dividindo</p>
+                </h3>
+                <h3 v-else class="text-center mt-4">{{ numero1 * numero2 }} <p class="mt-4">
+                        Você está multiplicando</p>
+                </h3>
+            </div>
+        </form>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
